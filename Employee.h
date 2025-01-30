f7b4508406c7012320289d182c4d8d8d91c609c0
AntonyArciolla <arciollaar@acad.sunyBroome.edu>
2025-01-30 10:47:39 -0500

    start

--- Employee.h
+++ Employee.h
@@ -1,5 +1,14 @@
 #pragma once
 class Employee
 {
+private:
+	std::string name;
+	std::string email;
+	std::string location;
+	struct skill {
+		std::string skill_name;
+		int skill_level;
+	};
+
 };
 
