af116b14854325cae6d6e4579d093c7eeb10205f
AntonyArciolla <Antony.arciolla1@gmail.com>
2025-02-02 21:16:30 -0500

    COOKING

--- Employee.cpp
+++ Employee.cpp
@@ -1,1 +1,14 @@
 #include "Employee.h"
+
+Employee::Employee(std::string name, std::string email)
+{
+
+}
+
+std::ostream& operator<<(std::ostream& os, const Employee& person)
+{
+	os << "Name: " << person.getName() << std::endl << "Email: " << person.getEmail() 
+		<< std::endl << "Location: " << person.getLocation() << std::endl;
+}
+
+
--- Employee.h
+++ Employee.h
@@ -1,14 +1,27 @@
 #pragma once
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+struct skill {
+	std::string skill_name;
+	int skill_level;
+};
 class Employee
 {
 private:
 	std::string name;
 	std::string email;
 	std::string location;
-	struct skill {
-		std::string skill_name;
-		int skill_level;
+
+	skill array[5]{
 	};
+public:
+	Employee(std::string name, std::string email);
+	std::string set_location(std::string newlocation);
+	std::string getLocation() const { return location; };
+	std::string getName() const { return name; };
+	std::string getEmail() const { return email; };
+	friend std::ostream& operator<<(std::ostream& os, const Employee& person );
 
 };
-
